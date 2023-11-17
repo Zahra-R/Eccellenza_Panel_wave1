0ac001ebe026b1b1cdac9c70564e4d2979596d88
daniel-4h <daniel.ahrndsen@unibas.ch>
2023-11-17 18:41:03 +0100

    Added multiple scales to questionnaire

--- Scales/__init__.py
+++ Scales/__init__.py
@@ -1,3 +1,5 @@
+### things so solve: Norms - Two different anchors on one page. All scales - center anchors over respective choices?
+
 import random
 
 from otree.api import *
@@ -47,13 +49,6 @@ class Group(BaseGroup):
 
 
 # custom function to automatically make likert type fields with 5 options
-def make_likert5(label):
-        return models.IntegerField(
-            choices=[1,2,3,4,5],
-            label=label,
-            widget=widgets.RadioSelect,
-            )
-
 # custom function to automatically make 4 option fields
 def make_options4(label):
         return models.IntegerField(
@@ -61,6 +56,43 @@ def make_options4(label):
             label=label,
             widget=widgets.RadioSelect,
             )
+
+def make_no_yes(label):
+        return models.IntegerField(
+            choices=[0,1],
+            label=label,
+            widget=widgets.RadioSelect,
+            )
+
+def make_likert5(label):
+        return models.IntegerField(
+            choices=[1,2,3,4,5],
+            label=label,
+            widget=widgets.RadioSelect,
+            )
+
+def make_likert7(label):
+        return models.IntegerField(
+            choices=[1,2,3,4,5,6,7],
+            label=label,
+            widget=widgets.RadioSelect,
+            )
+
+# 9-point likert scale with scaling for biospheric values
+def make_likert9(label):
+        return models.IntegerField(
+            choices=[-1,0,1,2,3,4,5,6,7],
+            label=label,
+            widget=widgets.RadioSelect,
+            )
+
+def make_likert10(label):
+        return models.IntegerField(
+            choices=[0,1,2,3,4,5,6,7,8,9,10],
+            label=label,
+            widget=widgets.RadioSelect,
+            )
+
 
 
 class Player(BasePlayer):
@@ -104,6 +136,55 @@ class Player(BasePlayer):
     cck12 = make_options4(Lexicon.cck12Label)
     cck13 = make_options4(Lexicon.cck13Label)
    
+    # (Dis)trust in climate science scale Sarathchandra & Haltinner, 2021
+    cst1 = make_likert5(Lexicon.cst1Label)
+    cst2 = make_likert5(Lexicon.cst2Label)
+    cst3 = make_likert5(Lexicon.cst3Label)
+    cst4 = make_likert5(Lexicon.cst4Label)
+    cst5 = make_likert5(Lexicon.cst5Label)
+    cst6 = make_likert5(Lexicon.cst6Label)
+
+    # Personal efficacy Leiserowitz et al, 2010
+    pe1 = make_likert7(Lexicon.pe1Label)
+    pe2 = make_likert7(Lexicon.pe2Label)
+    pe3 = make_likert7(Lexicon.pe3Label)
+
+    # Worldviews and values - Hierarchy-Egalitarianism & Individualism-Communitarianism  
+    wvv1 = make_likert7(Lexicon.wvv1Label)
+    wvv2 = make_likert7(Lexicon.wvv2Label)
+    wvv3 = make_likert7(Lexicon.wvv3Label)
+    wvv4 = make_likert7(Lexicon.wvv4Label)
+    wvv5 = make_likert7(Lexicon.wvv5Label)
+    wvv6 = make_likert7(Lexicon.wvv6Label)
+
+    # Importance of biospheric values van der Linden, 2015  
+    ibv1 = make_likert9(Lexicon.ibv1Label)
+    ibv2 = make_likert9(Lexicon.ibv2Label)
+    ibv3 = make_likert9(Lexicon.ibv3Label)
+    ibv4 = make_likert9(Lexicon.ibv4Label)
+
+    # Political Orientation Pennycook et al 2020
+    po1 = make_likert5(Lexicon.po1Label)
+    po2 = make_likert5(Lexicon.po2Label)
+
+    # Trust in political institutions from Eurobarometer / Lantian et al 2016  
+    pit1 = make_likert7(Lexicon.pit1Label)
+    pit2 = make_likert7(Lexicon.pit2Label)
+    pit3 = make_likert7(Lexicon.pit3Label)
+    pit4 = make_likert7(Lexicon.pit4Label)
+
+    # Trust in official version of events from Eurobarometer / Lantian et al 2016  
+    ovt1 = make_likert10(Lexicon.ovt1Label)
+
+    # Cognitive Reflection Task Thomson & Oppenheimer 2016
+    crt1 = make_likert7(Lexicon.pit1Label)
+    crt2 = make_likert7(Lexicon.pit2Label)
+    crt3 = make_likert7(Lexicon.pit3Label)
+    crt4 = make_likert7(Lexicon.pit4Label)
+
+    # Effort put into completing task
+    ecom1 = make_no_yes(Lexicon.ecom1Label)
+
 
 class CCConcern(Page):
     form_model = 'player'
@@ -132,6 +213,68 @@ class CCKnowledge(Page):
     @staticmethod
     def vars_for_template(player: Player):
         return dict(Lexicon=Lexicon, **which_language)
+    
+class CSTrust(Page):
+    form_model = 'player'
+    form_fields= ['cst1', 'cst2', 'cst3', 'cst4', 'cst5', 'cst6']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
 
+class PEfficacy(Page):
+    form_model = 'player'
+    form_fields= ['pe1', 'pe2', 'pe3']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
+    
+class WVValues(Page):
+    form_model = 'player'
+    form_fields= ['wvv1', 'wvv2', 'wvv3', 'wvv4', 'wvv5', 'wvv6']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
 
-page_sequence = [CCKnowledge, CCConcern, CCEmotion]
+class IBValues(Page):
+    form_model = 'player'
+    form_fields= ['ibv1', 'ibv2', 'ibv3', 'ibv4']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
+    
+class PolOrientation(Page):
+    form_model = 'player'
+    form_fields= ['po1', 'po2']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
+    
+class PITrust(Page):
+    form_model = 'player'
+    form_fields= ['pit1', 'pit2', 'pit3', 'pit4']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
+    
+class OVTrust(Page):
+    form_model = 'player'
+    form_fields= ['ovt1']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
+    
+class CRTask(Page):
+    form_model = 'player'
+    form_fields= ['crt1', 'crt2', 'crt3', 'crt4']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
+    
+class EffCompletion(Page):
+    form_model = 'player'
+    form_fields= ['ecom1']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(Lexicon=Lexicon, **which_language)
+
+page_sequence = [CCConcern, CCEmotion, CCKnowledge, CSTrust, PEfficacy, WVValues, IBValues, PolOrientation, PITrust, OVTrust, CRTask, EffCompletion]
--- Scales/lexicon_en.py
+++ Scales/lexicon_en.py
@@ -11,12 +11,23 @@ class Lexicon:
     very_much = "very much"
     no_effort = "no effort"
     great_deal_of_effort = "a great deal of effort"
+    not_important = "not important"
     not_at_all_important = "not at all important"
     extremely_important = "extremely important"
     likely_increase = "Likely to Increase"
     no_change = "No Change"
     likely_decrease = "Likely to Decrease"
     dont_know = "Don't know"
+    opposed_to_values = "opposed to my values"
+    strongly_liberal = "Strongly Liberal"
+    somewhat_liberal = "Somewhat Liberal"
+    moderate = "Moderate"
+    somewhat_conservative = "Somewhat Conservative"
+    strongly_conservative = "Strongly Conservative"
+    no_trust_at_all = "No trust at all"
+    full_trust = "Full trust"
+    completely_false = "Completeley False"
+    completely_true = "Completely True"
 
 
     #Climate Change Concern Tobler
@@ -67,3 +78,72 @@ class Lexicon:
     cck11Label = "The hole in the ozone layer" 
     cck12Label = "Frequency of hot days and nights" 
     cck13Label = "Global fresh water supply"
+
+
+# not yet inserted in de lexicon from here
+    # (Dis)trust in Climate Science Sarathchandra & Haltinner
+    cst_title = "Opinion on Climate Science"
+    cst_header = "How much do you agree with the following statements?"
+    cst1Label = "Climate scientists do not have enough data to know that human-caused climate change is happening."
+    cst2Label = "Climate scientists are not open about their research."
+    cst3Label = "Climate scientists ignore those who disagree with them."
+    cst4Label = "Scientific journals only publish papers that conclude climate change is happening."
+    cst5Label = "There is no scientific consensus that human-caused climate change is happening."
+    cst6Label = "Climate scientists can’t be trusted."
+
+    # Personal efficacy Leiserowitz et al, 2010
+    pe_title = "Attitudes about Climate Change"
+    pe_header = "How much do you agree with the following statements?"
+    pe1Label = "The actions of a single individual won't make any difference in climate change."
+    pe2Label = "Individuals are able to influence global warming and climate change outcomes."
+    pe3Label = "Individual actions will influence others to behave in ways that mitigate the effects of global warming."
+    
+    # Worldviews and values - Hierarchy-Egalitarianism & Individualism-Communitarianism  
+    wvv_title = "How do you see the world?"
+    wvv_header = "How much do you agree with the following statements?"
+    wvv1Label = "Our society would be better off if the distribution of wealth was more equal."
+    wvv2Label = "A lot of problems in our society come from the decline in the traditional family, where the man works and the woman stays home."
+    wvv3Label = "Discrimination against minorities is still a very serious problem in our country."
+    wvv4Label = "The government interferes far too much in our everyday lives."
+    wvv5Label = "I feel that people who are successful in business have a right to enjoy their wealth as they see fit."
+    wvv6Label = "Too many people expect society to do things for them that they should be doing for themselves."
+
+   # Importance of biospheric values van der Linden, 2015   
+    ibv_title = "Guiding principles in enviromental issues"
+    ibv_header = "How important are the following values important to you as guiding principles of your life?"
+    ibv1Label = "Respecting the Earth (harmony with other species)"
+    ibv2Label = "Protecting the Environment (preserving nature)"
+    ibv3Label = "Preventing Pollution (protecting natural resources)"
+    ibv4Label = "Unity with Nature (fitting into nature)"
+
+    # Political Orientation Pennycook et al 2020
+    po_title = "Political Orientation"
+    po_header = "What's your general stance in political issues?"
+    po1Label = "On social issues I am..."
+    po2Label = "On economic issues I am..."
+
+    # Trust in political institutions from Eurobarometer / Lantian et al 2016  
+    pit_title = "How much trust do you have in certain institutions?"
+    pit_header = "For each of the following institutions, do you tend to trust it or tend not to trust it?"
+    pit1Label = "The government"
+    pit2Label = "Regional or local public authorities"
+    pit3Label = "Justice and the legal system"
+    pit4Label = "The media"
+
+    # Trust in official version of events from Eurobarometer / Lantian et al 2016  
+    ovt_title = "Trust in the 'offical version' of events"
+    ovt_header = "Some political and social events are debated (for example 09/11 attacks, the death of Lady Diana, the assassination of John F. Kennedy). It is suggested that the 'official version' of these events could be an attempt to hide the truth to the public. This 'official version' could mask the fact that these events have been planned and secretly prepared by a covert alliance of powerful individuals or organizations (for example secret services or government). </br> What do you think? </br></br> To answer, please indicate to what extent the sentence below represents how you think about this:"
+    ovt1Label = "I think that the official version of the events given by the authorities very often hides the truth."
+
+    # Cognitive Reflection Task Version 2 Thomson, & Oppenheimer, 2016 
+    crt_title = ""
+    crt_header = "Please solve the following problems:"
+    crt1Label = "If you're running a race and you pass the person in second place, what place are you in?"
+    crt2Label = "A farmer had 15 sheep and all but 8 died. How many are left?"
+    crt3Label = "Emily's father has three daughters. The first two are named April and May. What is the third daughter's name?"
+    crt4Label = "How many cubic feet of dirt are there in a hole that is 3' deep x 3' wide x 3' long?). "
+
+    # Trust in official version of events from Eurobarometer / Lantian et al 2016  
+    ecom_title = "Hand on heart!"
+    ecom_header = "Directly asking: How much effort did you put into completing this task? Did you simply click through the options or did you think about your answers? </br></br><i>Please answer honestly, this will not affect your compensation or your ability to complete the study. We need to know this to analyze the data properly.</i>"
+    ecom1Label = "You can use my answers for your analysis."
