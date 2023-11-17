915c3695d162bbcc5dc1eaafeaf1be87db2d4c36
daniel-4h <daniel.ahrndsen@unibas.ch>
2023-11-17 20:26:34 +0100

    Lexicon corrected, CRTask debugged

--- Scales/lexicon_en.py
+++ Scales/lexicon_en.py
@@ -139,8 +139,8 @@ class Lexicon:
     crt1Label = "If you're running a race and you pass the person in second place, what place are you in?"
     crt2Label = "A farmer had 15 sheep and all but 8 died. How many are left?"
     crt3Label = "Emily's father has three daughters. The first two are named April and May. What is the third daughter's name?"
-    crt4Label = "How many cubic feet of dirt are there in a hole that is 3' deep x 3' wide x 3' long?). "
-
+    crt4Label = "How many cubic feet of dirt are there in a hole that is 3' deep x 3' wide x 3' long?)."
+    
     # Trust in official version of events from Eurobarometer / Lantian et al 2016  
     ecom_title = "Hand on heart!"
     ecom_header = "Directly asking: How much effort did you put into completing this task? Did you simply click through the options or did you think about your answers? </br></br><i>Please answer honestly, this will not affect your compensation or your ability to complete the study. We need to know this to analyze the data properly.</i>"
