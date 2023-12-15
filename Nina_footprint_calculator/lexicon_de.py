62b078614fd56772b22a48757f4884122c136ab8
Nina Frings <nina.frings@unibas.ch>
2023-12-15 16:42:17 +0100

    filesNina

---
+++ Nina_footprint_calculator/lexicon_de.py
@@ -0,0 +1,66 @@
+class Lexicon:
+
+    title = ' Verhalten '
+    intro_a = 'In diesem nächsten Abschnitt sammeln wir Informationen zu einigen Ihrer tatsächlichen Verhaltensweisen. <br>Bitte antworten Sie so genau wie möglich. Vielen Dank im Voraus!'
+    
+    regional_label = '<b> Welcher Prozentsatz Ihrer Lebensmittel stammt aus der Region (aus Ihrem Land oder Ihrer Region, nicht importiert)? </b>'
+    regional_less_than = 'Weniger als ein Viertel'
+    regional_quarter= 'Etwa ein Viertel'
+    regional_half = 'Etwa die Hälfte'
+    regional_3_quarter = 'Etwa drei Viertel'
+    regional_more_than = 'Der größte Teil stammt aus der Region'
+
+    electricity_label = '<b>Diese Frage bezieht sich auf Ihre Elektrizitätsversorgung. Wie sieht Ihre Stromversorgung aus?</b>'
+    electricity_D = 'Ich habe ausschließlich grüne Energie'
+    electricity_C = 'Ich habe teilweise grüne Energie (gemischt)' 
+    electricity_B = 'Ich habe ausschließlich konventionelle (fossile) Energie'
+    electricity_A = 'Ich weiß es nicht'
+
+
+    food_dairy_label = '<b>Wie oft konsumieren Sie Milchprodukte wie Joghurt, Käse, Butter oder Sahne?</b>'
+    food_dairy_less_than_A = 'Weniger als einmal pro Woche oder nie'
+    food_dairy_A_to_B = '1-3 Mal pro Woche'
+    food_dairy_B_to_C = '4-6 Mal pro Woche'
+    food_dairy_C_to_D= '1-2 Mal täglich'
+    food_dairy_D_to_E = '2-4 Mal täglich'
+    food_dairy_more_than_E = 'Mehr als vier Mal täglich'
+
+    food_meat_label = '<b> Wie oft essen Sie Mahlzeiten mit Fleisch oder Fisch (z.B. Spaghetti Bolognese, Lachsrolle usw.)</b>?'
+    food_meat_less_than_A = 'Weniger als einmal pro Woche oder nie'
+    food_meat_A_to_B= '1-3 Mal pro Woche'
+    food_meat_B_to_C = '4-6 Mal pro Woche'
+    food_meat_C_to_D = 'Einmal täglich'
+    food_meat_D_to_E = '2-3 Mal täglich'
+    food_meat_more_than_E = 'Mehr als drei Mal täglich'
+
+    commute_pt_label = '<b> Wie viele Kilometer legen Sie <b> wöchentlich mit öffentlichen Verkehrsmitteln </b> (Zug, Bus, usw.) oder mit einem E-Bike zurück? Bitte berücksichtigen Sie alle privaten Fahrten einschließlich des Arbeitswegs, jedoch ohne Dienstreisen.</b>'
+
+    commute_pt_never = 'Ich nehme nie öffentliche Verkehrsmittel'
+    commute_pt_less_than_A = '1 - 60 km'
+    commute_pt_A_to_B= '60 - 80 km'
+    commute_pt_B_to_C = '80 -239 km'
+    commute_pt_C_to_D = '240 - 359 km'
+    commute_pt_D_to_E = '360 - 600 km'
+    commute_pt_more_than_E = 'Mehr als 600 km'
+
+
+    commute_car_label = '<b> Wie viele Kilometer fahren Sie <b> jährlich mit dem Auto </b> oder einem Motorrad (ausserhalb der Arbeit, sowohl als Fahrer als auch als Beifahrer)? </b>'
+    commute_car_never = 'Ich benutze kein Auto oder Motorrad'
+    commute_car_less_than_A = '1 - 2,000 km'
+    commute_car_A_to_B= '2,000 - 7,499 km'
+    commute_car_B_to_C = '7,500 - 12,499 km'
+    commute_car_C_to_D = '12,500 - 30,000 km'
+    commute_car_more_than_D = 'Mehr als 30,000 km'
+
+    commute_car_type_label = '<b> Mit welcher Art von Kraftstoff wird Ihr Auto betrieben? </b>'
+    commute_car_type_none = 'Ich habe kein Auto'
+    commute_car_type_E = 'Benzin/Diesel/Hybrid'
+    commute_car_type_D = 'Erdgas'
+    commute_car_type_C = 'Biogas'
+    commute_car_type_B = 'Elektrisch (konventionelle Energie)'
+    commute_car_type_A = 'Elektrisch (grüne Energie)'   
+    
+
+    flying_short_label = '<b> Wie viele Kurzstreckenflüge (<3 Stunden) haben Sie in den letzten zwei Jahren im Durchschnitt genommen?</b> <i> i: Ein Hin- und Rückflug zählt als zwei Flüge. Wenn Sie also von Paris nach London und zurück geflogen sind, zählt das als 2 Flüge. </i> '
+    flying_mid_label ='<b> Wie viele Mittelstreckenflüge (3-6 Stunden) haben Sie in den letzten zwei Jahren im Durchschnitt genommen?</b> <i> i: Ein Hin- und Rückflug zählt als zwei Flüge. Wenn Sie also von New York nach San Francisco und zurück geflogen sind, zählt das als 2 Flüge. </i> '
+    flying_long_label = '<b> Wie viele Langstreckenflüge  (>6 hours) haben Sie in den letzten zwei Jahren im Durchschnitt genommen? </b> <i> i: Ein Hin- und Rückflug zählt als zwei Flüge.  Wenn Sie also von London nach New York und zurück geflogen sind, zählt das als 2 Flüge. </i> '
