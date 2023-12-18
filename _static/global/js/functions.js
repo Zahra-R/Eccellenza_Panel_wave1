69e8ba1a3c3c84e9ed3065626dba69fd0ddbe364
Zahra <zahra.rahmani@hotmail.de>
2023-12-18 16:15:46 +0100

    very specific column widths

--- _static/global/js/functions.js
+++ _static/global/js/functions.js
@@ -1,5 +1,5 @@
 
-function setWidthForTableColumns1() {
+function old() {
     // Get all table rows
     var rows = document.querySelectorAll('.radiotable tr');
 
@@ -10,7 +10,6 @@ function setWidthForTableColumns1() {
     rows.forEach(function(row) {
         // Get all cells in the row, excluding the first one
         var cells = row.querySelectorAll('th:not(:first-child), td:not(:first-child)');
-
         // Iterate over each cell in the row
         cells.forEach(function(cell) {
             // Get the content of the cell
@@ -25,6 +24,7 @@ function setWidthForTableColumns1() {
             // Update the min width for the entire table
             minTableWidth = Math.max(minTableWidth, maxLength);
         });
+        
     });
 
     // Apply the min width to each column (excluding the first one)
@@ -34,12 +34,22 @@ function setWidthForTableColumns1() {
 
         // Iterate over each cell in the row
         cells.forEach(function(cell) {
-            cell.style.minWidth = minTableWidth + 'ch';
+            if(minTableWidth>=5){
+                cell.style.maxWidth = 6 + 'ch';
+                cell.style.minWidth = 6 + 'ch';
+                console.log("is small")
+            }
+            else {
+                cell.style.maxWidth = 6 + 'ch';
+                cell.style.minWidth = 6 + 'ch';
+                console.log("is large")
+            }
+            
         });
     });
 }
 
-function setWidthForTableColumns2() {
+function old2() {
     // Get all table rows
     var rows = document.querySelectorAll('.radiotable tr');
 
@@ -74,9 +84,94 @@ function setWidthForTableColumns2() {
 
         // Iterate over each cell in the row
         cells.forEach(function(cell) {
-            cell.style.minWidth = minTableWidth + 'ch';
+            if(minTableWidth>=5){
+                cell.style.maxWidth = 6 + 'ch';
+                cell.style.minWidth = 6 + 'ch';
+            }
+            else {
+                cell.style.minWidth = minTableWidth + 'ch';
+            }
         });
     });
 }
 
+
+function setWidthForTableColumns3() {
+    var rows = document.querySelectorAll('.radiotable tr');
+    rows.forEach(function(row) {
+        // Get all cells in the row, excluding the first one
+        var cells = row.querySelectorAll('th:not(:first-child), td:not(:first-child)');
+
+        // Iterate over each cell in the row
+        cells.forEach(function(cell) {
+                cell.style.maxWidth = 10 + 'ch';
+                cell.style.minWidth = 10 + 'ch';
+            
+        });
+    });
+}
+
+
+function setWidthForTableColumns1() {
+    var rows = document.querySelectorAll('.radiotable tr');
+    rows.forEach(function(row) {
+        // Get all cells in the row, excluding the first one
+        var cells = row.querySelectorAll('th:not(:first-child), td:not(:first-child)');
+
+        // Iterate over each cell in the row
+        cells.forEach(function(cell) {
+                cell.style.maxWidth = 6 + 'ch';
+                cell.style.minWidth = 6 + 'ch';
+            
+        });
+    });
+}
+
+
+function setWidthForTableColumns7() {
+    var rows = document.querySelectorAll('.radiotable tr');
+    rows.forEach(function(row) {
+        // Get all cells in the row, excluding the first one
+        var cells = row.querySelectorAll('th:not(:first-child), td:not(:first-child)');
+
+        // Iterate over each cell in the row
+        cells.forEach(function(cell) {
+                cell.style.maxWidth = 7 + 'ch';
+                cell.style.minWidth = 7 + 'ch';
+            
+        });
+    });
+}
+
+
+function setWidthForTableColumns2() {
+    var rows = document.querySelectorAll('.radiotable tr');
+    rows.forEach(function(row) {
+        // Get all cells in the row, excluding the first and second ones
+        var cells = row.querySelectorAll('th:not(:nth-child(1)):not(:nth-child(2)), td:not(:nth-child(1)):not(:nth-child(2))');
+
+        // Iterate over each cell in the row
+        cells.forEach(function(cell) {
+                cell.style.maxWidth = 6 + 'ch';
+                cell.style.minWidth = 6 + 'ch';
+            
+        });
+    });
+}
+
+
+function setWidthForTableColumns4() {
+    var rows = document.querySelectorAll('.radiotable tr');
+    rows.forEach(function(row) {
+        // Get all cells in the row, excluding the first and second ones
+        var cells = row.querySelectorAll('th:not(:nth-child(1)):not(:nth-child(2)), td:not(:nth-child(1)):not(:nth-child(2))');
+
+        // Iterate over each cell in the row
+        cells.forEach(function(cell) {
+                cell.style.maxWidth = 5 + 'ch';
+                cell.style.minWidth = 5 + 'ch';
+            
+        });
+    });
+}
 console.log("Check!");
