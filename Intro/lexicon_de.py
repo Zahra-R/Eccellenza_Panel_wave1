961ceedef1f70be6e073bc548614f29c83ce1a36
Zahra <zahra.rahmani@hotmail.de>
2024-01-10 10:43:43 +0100

    put (old!) consent form in smaller chunks

--- Intro/lexicon_de.py
+++ Intro/lexicon_de.py
@@ -1,19 +1,54 @@
 class Lexicon:
     information_title = 'Informationen zu dieser Studie'
-    instructions = "Instruktionen"
-    no = "Nein"
-    results = "Ergebnis"
-    round_number = "Runde"
-    start = "Start"
-    stop = "Stopp"
-    yes = "Yes"
-    your_decision = "Ihre Entscheidung"
-    not_at_all = "überhaupt nicht"
-    very_much = "sehr stark"
-    next = "Weiter"
+    instructions = 'Instruktionen'
+    no = 'Nein'
+    results = 'Ergebnis'
+    round_number = 'Runde'
+    start = 'Start'
+    stop = 'Stopp'
+    yes = 'Yes'
+    your_decision = 'Ihre Entscheidung'
+    not_at_all = 'überhaupt nicht'
+    very_much = 'sehr stark'
+    next = 'Weiter'
 
     #Consent Text
-    consentText ='<p>Sie wurden eingeladen, an einer Forschungsstudie teilzunehmen, um verschiedene Aussagen zu lesen und zu bewerten. Diese Studie wird unter der Leitung von Prof. Ulf J.J. Hahnel, Fakultät für Psychologie, Universität Basel, Schweiz, und Zahra Rahmani Azad, Fakultät für Psychologie, Universität Basel, durchgeführt.</p> <p>Wenn Sie an dieser Studie teilnehmen möchten, werden Sie gebeten, Folgendes zu tun:    <ul>     <li>Einen Satz von Online-Fragebögen auszufüllen. Möglicherweise werden Sie gebeten, zu bewerten, wie gut bestimmte Aussagen oder Persönlichkeitsmerkmale auf Sie zutreffen.</li>     <li>Sie werden auch gebeten, einige demografische Informationen anzugeben, darunter Ihr Alter, Geschlecht, politische Parteilichkeit und Einkommen.</li>     <li>Sie treffen mehrere Entscheidungen zwischen zwei Optionen mit unterschiedlichen finanziellen Auswirkungen. <b>Ihre Bonuszahlung für diese Studie hängt von den Entscheidungen ab, die Sie in jedem Versuch treffen, und kann variieren.</b></li>   </ul> </p> <p><strong>Dauer:</strong> Die Teilnahme an dieser Studie erfordert etwa <b>10 Minuten</b> Ihrer Zeit.</p> <p><strong>Risiken und Vorteile:</strong> Es sind keine bekannten Risiken im Zusammenhang mit Ihrer Teilnahme an dieser Forschung bekannt, die über die des täglichen Lebens hinausgehen. Sie werden über Prolific für die Teilnahme an dieser Studie incentiviert. Ihre Teilnahme kann dazu beitragen, offene Forschungsfragen in der Verhaltens- und Umweltpsychologie besser zu verstehen. Ihre Vergütung umfasst eine <b>feste Zahlung von £1,50 (was etwa USD $1,83 entspricht)</b>. Zusätzlich dazu, je nach Ihren Entscheidungen während der Studie, <b>können Sie eine zusätzliche Bonuszahlung von bis zu £1,25 (was etwa USD $1,52 entspricht) erhalten oder auch nicht.</b></p> <p>Endauszahlung = Festbetrag + Bonus</p> <p><strong>Datenschutz:</strong> Die Vertraulichkeit Ihrer Daten wird streng durch die Zuweisung von Codenummern an jeden Teilnehmer gewährleistet, sodass die Daten niemals direkt mit der individuellen Identität verknüpft werden. Niemand, einschließlich der Forscher dieser Studie, wird in der Lage sein, Ihre Identität offenzulegen. Die Daten werden in anonymisierter Form auf Open-Science-Plattformen verfügbar gemacht, um es anderen Forschern zu ermöglichen, die Daten für ihre Forschungszwecke zu nutzen. Alle sensiblen Informationen, die potenziell zur Offenlegung Ihrer Identität verwendet werden könnten, werden aus dem Datensatz entfernt, bevor die Daten veröffentlicht werden. Wir, die Forscher des Projekts, werden auch niemals die Identität eines der Teilnehmer kennen, es sei denn, Sie offenbaren sie uns, indem Sie uns per E-Mail kontaktieren.</p> <p><strong>Die Teilnahme an dieser Studie ist freiwillig. Sie können jederzeit ohne Nachteile ablehnen oder sich zurückziehen.</strong> Sie erhalten nur eine Zahlung für die Teilnahme, wenn Sie die Studie bis zum Ende abschließen.</p> <p>Wenn Ihnen etwas an der Studie oder Ihrer Teilnahme unklar ist, wenn Sie Fragen haben oder ein forschungsbezogenes Problem melden möchten, verwenden Sie bitte das Nachrichtentool in Prolific oder senden Sie eine E-Mail an zahra.rahmani@unibas.ch.</p> <p>Indem Sie auf "Weiter" klicken, erklären Sie Ihr Einverständnis zur Teilnahme an dieser Studie und geben an, dass Sie   <ul>     <li>mindestens 18 Jahre alt sind</li>     <li>die angegebenen Bedingungen gelesen und verstanden haben und genügend Zeit hatten, um eine Entscheidung zu treffen.</li>   </ul>   Wenn Sie teilnehmen möchten, müssen Sie die unten stehenden Kästchen ankreuzen und auf "Weiter" klicken. <b>Wenn Sie nicht teilnehmen möchten, schließen Sie einfach dieses Fenster.</b> </p> <p>Aufgrund der oben präsentierten Informationen bestätige ich, dass ich an dieser Studie teilnehmen möchte, und ich autorisiere:</p> '
+    consent_p1 ='Sie wurden eingeladen, an einer Forschungsstudie teilzunehmen, um verschiedene Aussagen zu lesen und zu bewerten. Diese Studie wird unter der Leitung von Prof. Ulf J.J. Hahnel, Fakultät für Psychologie, Universität Basel, Schweiz, und Zahra Rahmani Azad, Fakultät für Psychologie, Universität Basel, durchgeführt.'
+    consent_p2 = 'Wenn Sie an dieser Studie teilnehmen möchten, werden Sie gebeten, Folgendes zu tun:'
+
+    consent_bullet1 = 'Einen Satz von Online-Fragebögen auszufüllen. Möglicherweise werden Sie gebeten, zu bewerten, wie gut bestimmte Aussagen oder Persönlichkeitsmerkmale auf Sie zutreffen.'
+    consent_bullet2= 'Sie werden auch gebeten, einige demografische Informationen anzugeben, darunter Ihr Alter, Geschlecht, politische Parteilichkeit und Einkommen.'
+    consent_bullet3 = 'Sie treffen mehrere Entscheidungen zwischen zwei Optionen.'
+    
+    
+    ## Achtung hier richtige dauer
+    duration ='Dauer'
+    consent_p3 ='Die Teilnahme an dieser Studie erfordert etwa <b>10 Minuten</b> Ihrer Zeit.'
+
+    ## Achtung hier richtige vergütung und währung und marktforschungsinstitut
+    risks = 'Risiken und Vorteile:'
+    consent_p4 = 'Es sind keine bekannten Risiken im Zusammenhang mit Ihrer Teilnahme an dieser Forschung bekannt, die über die des täglichen Lebens hinausgehen. Sie werden über Prolific für die Teilnahme an dieser Studie incentiviert. Ihre Teilnahme kann dazu beitragen, offene Forschungsfragen in der Verhaltens- und Umweltpsychologie besser zu verstehen. Ihre Vergütung beträgt £1,50 (was etwa USD $1,83 entspricht).'
+
+    privacy = 'Datenschutz:'
+    consent_p5 = 'Die Vertraulichkeit Ihrer Daten wird streng durch die Zuweisung von Codenummern an jeden Teilnehmer gewährleistet, sodass die Daten niemals direkt mit der individuellen Identität verknüpft werden. Niemand, einschließlich der Forscher dieser Studie, wird in der Lage sein, Ihre Identität offenzulegen. Die Daten werden in anonymisierter Form auf Open-Science-Plattformen verfügbar gemacht, um es anderen Forschern zu ermöglichen, die Daten für ihre Forschungszwecke zu nutzen. Alle sensiblen Informationen, die potenziell zur Offenlegung Ihrer Identität verwendet werden könnten, werden aus dem Datensatz entfernt, bevor die Daten veröffentlicht werden. Wir, die Forscher des Projekts, werden auch niemals die Identität eines der Teilnehmer kennen, es sei denn, Sie offenbaren sie uns, indem Sie uns per E-Mail kontaktieren.'
+
+    consent_p6 = 'Die Teilnahme an dieser Studie ist freiwillig. Sie können jederzeit ohne Nachteile ablehnen oder sich zurückziehen.'
+
+    consent_p7 = 'Sie erhalten nur eine Zahlung für die Teilnahme, wenn Sie die Studie bis zum Ende abschließen.'
+
+
+    ### achtung achtung, hier prolific
+    consent_p8 = 'Wenn Ihnen etwas an der Studie oder Ihrer Teilnahme unklar ist, wenn Sie Fragen haben oder ein forschungsbezogenes Problem melden möchten, verwenden Sie bitte das Nachrichtentool in Prolific oder senden Sie eine E-Mail an zahra.rahmani@unibas.ch.'
+
+    consent_p9 = 'Indem Sie auf "Weiter" klicken, erklären Sie Ihr Einverständnis zur Teilnahme an dieser Studie und geben an, dass Sie '
+
+    consent_bullet4 = 'mindestens 18 Jahre alt sind'
+    consent_bullet5 = 'die angegebenen Bedingungen gelesen und verstanden haben und genügend Zeit hatten, um eine Entscheidung zu treffen.'
+
+    consent_p10 = 'Wenn Sie teilnehmen möchten, müssen Sie die unten stehenden Kästchen ankreuzen und auf "Weiter" klicken. <b>Wenn Sie nicht teilnehmen möchten, schließen Sie einfach dieses Fenster.</b>'
+    consent_p11 = 'Aufgrund der oben präsentierten Informationen bestätige ich, dass ich an dieser Studie teilnehmen möchte, und ich autorisiere:'
+
+   
     data_consent1 = 'Die Nutzung der Daten für wissenschaftliche Zwecke und/oder die Publikation der Ergebnisse dieser Studie in wissenschaftlichen Zeitschriften oder Büchern jeweils unter der Voraussetzung, dass die Daten anonymisiert werden und keine Rückschlüsse auf meine Identität gezogen werden können.'
     data_consent2 = 'Die Nutzung der Daten für Lehrzwecke (Kurse und Vorträge für Studierende und Berufstätige, die jeweils zu einem vertraulichen Umgang verpflichtet sind).'
     
