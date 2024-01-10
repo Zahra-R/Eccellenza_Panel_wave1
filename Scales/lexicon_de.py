1f02a2ee6502ec1580693b4ec9b048b176b4b712
NinaLF <78842378+NinaLF@users.noreply.github.com>
2024-01-10 12:07:46 +0100

    Update lexicon_de.py
    Translation changes as indicated by excel file
    to cc concern-skepticism and biospheric values

--- Scales/lexicon_de.py
+++ Scales/lexicon_de.py
@@ -34,21 +34,21 @@ class Lexicon:
     ccc_header = "Wie sehr stimmen Sie den folgenden Aussagen zu?"
     ccc1Label = "Wir müssen das empfindliche Klimagleichgewicht schützen."
     ccc2Label = "Der Klimaschutz ist wichtig für unsere Zukunft."
-    ccc3Label = "Ich bin besorgt über den Zustand des Klimas."
+    ccc3Label = "Ich mache mir Sorgen über den Zustand des Klimas."
     ccc4Label = "Der Klimawandel hat ernste Folgen für die Menschheit und die Natur."
-    ccc10Label = "Der Klimawandel und seine Folgen werden in den Medien übertrieben dargestellt."
-    ccc11Label = "Der Klimawandel ist in erster Linie Geschäftemacherei."
-    ccc12Label = "Solange die Metereologie nicht mal das Wetter richtig vorhersagen kann, kann das Klima auch nicht zuverlässig vorhergesagt werden."
+    ccc10Label = "Der Klimawandel und seine Folgen werden in den Medien übertrieben."
+    ccc11Label = "Der Klimawandel ist ein betrügerisches Geschäft."
+    ccc12Label = "Solange Metereologen nicht einmal richtig das Wetter vorhersagen können, kann das Klima auch nicht zuverlässig vorhergesagt werden."
     ccc13Label = "Es gibt wichtigere Probleme als den Klimaschutz."
-    ccc14Label = "Ich fühle mich von dem Klimawandel nicht bedroht."
-    ccc15Label = "Die Folgen des Klimawandels sind unvorhersehbar, deswegen ist klimafreundliches Verhalten nutzlos."
-    ccc16Label = "Klimaschutz verhindert unnötigerweise unser Wirtschaftswachstum."
+    ccc14Label = "Ich fühle mich nicht von dem Klimawandel bedroht."
+    ccc15Label = "Die Folgen des Klimawandels sind unvorhersehbar, deswegen ist mein klimafreundliches Verhalten vergeblich."
+    ccc16Label = "Klimaschutz behindert unnötigerweise das Wirtschaftswachstum."
 
     #Climate Change Emotion Knauf/Truelove
     cce_title =  "Einstellungen zum Klimawandel"
-    cce_header = "Wenn ich an den Klimawandel danke, dann fühle ich..."
+    cce_header = "Wenn ich an den Klimawandel denke, dann fühle ich..."
     cce1Label = "Wut"
-    cce2Label = "Angst/Sorge"
+    cce2Label = "Angst"
     cce3Label = "Traurigkeit"
     cce4Label = "Freude"
     cce5Label = "Neugier"
@@ -60,7 +60,7 @@ class Lexicon:
     gwn1Label = "Wie viel Aufwand betreibt Ihre Familie und Ihre Freunde, um die Erderwärmung zu reduzieren?" #"How much of an effort do your family and friends make to reduce global warming?"
     gwn2Label = "Wie wichtig ist es für Ihre Familie und Ihre Freunde, dass Sie tätig werden, um die Erdwerärmung zu reduzieren?" #"How important is it to your family and friends that you take action to reduce global warming?"
 
-    ### Climate Change Impact / Consequence Knowledge van der Linden 
+    ### Climate Change Impact / Consequence Knowledge van der Linden is out different items
     cck_title = "Wissen über den Klimawandel"
     cck_header = "Bitte geben Sie an, ob die folgenden Größen voraussichtlich aufgrund des Klimawandels größer, kleiner oder gleich bleiben werden."
     cck1Label = "Weltweiter Meeresspiegel"
@@ -77,7 +77,7 @@ class Lexicon:
     cck12Label = "Häufigkeit von heißen Tagen und Nächten"
     cck13Label = "Weltweiter Süßwasservorrat"
 
-    # (Dis)trust in Climate Science Sarathchandra & Haltinner
+    # (Dis)trust in Climate Science Sarathchandra & Haltinner is out
     cst_title = "Meinung zu Klimawissenschaft"
     cst_header = "Wie sehr stimmen Sie den folgenden Aussagen zu?"
     cst1Label = "Klimawissenschaftler haben nicht genug Daten, um zu wissen, dass der vom Menschen verursachte Klimawandel existiert."
@@ -107,10 +107,10 @@ class Lexicon:
     # Importance of biospheric values van der Linden, 2015
     ibv_title = "Wichtige Prinzipien in Umweltfragen"
     ibv_header = "Wie wichtig sind Ihnen die folgenden Werte als generelle Leitprinzipien in Ihrem Leben?"
-    ibv1Label = "Ehrfurcht vor der Erde (Harmonie mit anderen Arten)"
-    ibv2Label = "Schutz der Umwelt (Erhaltung der Natur)"
-    ibv3Label = "Verhinderung von Verschmutzung (Schutz natürlicher Ressourcen)"
-    ibv4Label = "Einheit mit der Natur (Integration in die Natur)"
+    ibv1Label = "Respektieren der Erde (Harmonie mit anderen Arten)"
+    ibv2Label = "Schützen der Umwelt (die Natur erhalten)"
+    ibv3Label = "Verhindern von Umweltverschmutzung (natürlichen Ressourcen schützen)"
+    ibv4Label = "Einheit mit der Natur (sich in die Natür einfügen)"
 
     # Political Orientation Pennycook et al 2020
     po_title = "Politische Ausrichtung"
