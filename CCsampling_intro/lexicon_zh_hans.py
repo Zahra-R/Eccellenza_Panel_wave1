c3afa751ef275030fd3ea1ea4c6a14b31a959ef0
Nina Frings <nina.frings@unibas.ch>
2024-02-05 10:08:32 +0100

    Lexicon for chinese everywhere but  english text

---
+++ CCsampling_intro/lexicon_zh_hans.py
@@ -0,0 +1,16 @@
+class Lexicon:
+    next = "next"
+    not_at_all = "not at all"
+    very_much = "very much"
+
+    # Introduction
+    studyInformation = "Information about this study"
+    instructionCondition_telling_box_label_true = "On the next pages, you will find two boxes. Each box contains a number of <b>statements about climate change.</b><b>One box contains statements with arguments against immediate climate action. The other box contains statements arguing in favor of urgent climate action.</b>"
+    instructionCondition_telling_box_label_false = "On the next pages, you will find two boxes. Each box contains a number of <b>statements about climate change </b> coming from different sources."
+    instructionUnconditional1 = "When you click on one of the boxes, one statement will be randomly drawn from that box. Your task will be to read the statement and rate what you think about the statement. You will repeat this for <i>"
+    instructionUnconditional2 = "rounds</i>. In each round, you can decide from which box you want to read the next statement.</p><p> We know that climate change is a  complicated topic and there is a lot of different information about it out there. We are interested how you deal with the information environment and we are curious to see how you search for information. Please take your time to read each statement carefully and give your honest opinion. "
+
+    #beforeTask
+    ccconcern_title = "Your attitude on climate change"
+    ccconcern_instruction = "Please move the slider to reflect your attitude."
+    ccconcern_question = "How concerned are you about climate change?"
