30df8cdfbf99e53b7f263ab833e99b3f9fa14937
Zahra <zahra.rahmani@hotmail.de>
2024-02-08 10:05:17 +0100

    add separate scales app for jessis task

---
+++ Scales_Jessi/lexicon_en.py
@@ -0,0 +1,270 @@
+class Lexicon:
+    instructions = "Instructions"
+    no = "No"
+    results = 'Results'
+    round_number = "Round no."
+    start = "Start"
+    stop = "Stop"
+    yes = "Yes"
+    your_decision = "Your decision"
+    not_at_all = "not at all"
+    very_much = "very much"
+    no_effort = "no effort"
+    great_deal_of_effort = "a great deal of effort"
+    not_important = "not important"
+    not_at_all_important = "not at all important"
+    extremely_important = "extremely important"
+    likely_increase = "Likely to Increase"
+    no_change = "No Change"
+    likely_decrease = "Likely to Decrease"
+    dont_know = "Don't know"
+    opposed_to_values = "opposed to my values"
+    strongly_liberal = "Strongly Liberal"
+    somewhat_liberal = "Somewhat Liberal"
+    moderate = "Moderate"
+    somewhat_conservative = "Somewhat Conservative"
+    strongly_conservative = "Strongly Conservative"
+    no_trust_at_all = "No trust at all"
+    full_trust = "Full trust"
+    completely_false = "Completeley False"
+    completely_true = "Completely True"
+
+
+    #Climate Change Concern Tobler
+    ccc_title = "Attitudes about Climate Change"
+    ccc_header = "How much do you agree with the following statements?"
+    ccc1Label = "We must protect the climate’s delicate equilibrium."
+    ccc2Label = "Climate protection is important for our future."
+    ccc3Label = "I worry about the climate’s state."
+    ccc4Label = "Climate change has severe consequences for humans and nature."
+    ccc10Label = "Climate change and its consequences are being exaggerated in the media."
+    ccc11Label = "Climate change is a racket."
+    ccc12Label = "As long as meteorologists are not even able to accurately forecast weather, climate cannot be reliably predicted either."
+    ccc13Label = "There are larger problems than climate protection."
+    ccc14Label = "I do not feel threatened by climate change."
+    ccc15Label = "The impacts of climate change are unpredictable; thus, my climate-friendly behavior is futile."
+    ccc16Label = "Climate protection needlessly impedes economic growth."
+
+
+    #Climate Change Emotion only for jessi pre-test 
+    cce_title = "Attitudes about Climate Change"
+    cce_header = "When I think of Climate Change I feel..."
+    cce1Label = "Anger"
+    cce2Label = "Fear"
+    cce3Label = "Sadness"
+    cce4Label = "Joy"
+    cce5Label = "Curiosity"
+    cce6Label = "Hope"
+
+
+    # new emotions
+    #Climate Change Emotion
+
+    cceA1Label = "angry"
+    cceA2Label = "mad"
+    cceA3Label = "irritated"
+    cceF1Label = "fearful"
+    cceF2Label = "afraid"
+    cceF3Label = "scared"
+    cceS1Label = "sad"
+    cceS2Label = "sorrowful"
+    cceS3Label = "unhappy"
+    cceH1Label = "hopeful"
+    cceH2Label = "optimistic"
+    cceH3Label = "upbeat"
+    cceG1Label = "guilty"
+    cceG2Label = "regretful"
+    cceG3Label = "remorseful"
+
+    # Personal efficacy Leiserowitz et al, 2010
+    #Only item 1
+    pe_title = "Attitudes about Climate Change"
+    pe_header = "How much do you agree with the following statements?"
+    pe1Label = "The actions of a single individual won't make any difference in climate change."
+  
+    
+    # Worldviews and values - Hierarchy-Egalitarianism & Individualism-Communitarianism  
+    ## TBD kevin tam
+    wvv_title = "How do you see the world?"
+    wvv_header = "How much do you agree with the following statements?"
+    wvv1Label = "Our society would be better off if the distribution of wealth was more equal."
+    wvv2Label = "A lot of problems in our society come from the decline in the traditional family, where the man works and the woman stays home."
+    wvv3Label = "Discrimination against minorities is still a very serious problem in our country."
+    wvv4Label = "The government interferes far too much in our everyday lives."
+    wvv5Label = "I feel that people who are successful in business have a right to enjoy their wealth as they see fit."
+    wvv6Label = "Too many people expect society to do things for them that they should be doing for themselves."
+
+   # Importance of biospheric values van der Linden, 2015   
+    ibv_title = "Guiding principles in enviromental issues"
+    ibv_header = "How important are the following values to you as guiding principles of your life?"
+    ibv1Label = "Respecting the Earth (harmony with other species)"
+    ibv2Label = "Protecting the Environment (preserving nature)"
+    ibv3Label = "Preventing Pollution (protecting natural resources)"
+    ibv4Label = "Unity with Nature (fitting into nature)"
+
+    # Political Orientation Pennycook et al 2020
+    po_title = "Political Orientation"
+    po_header = "What's your general stance in political issues?"
+    po1Label = "On social issues I am..."
+    po2Label = "On economic issues I am..."
+
+    # Trust  in institutions in terms of cc Based on (Pan et al., 2023)
+    pit_title = "Trust in regards to climate change"
+    pit_header = "How much do you trust these actors in climate change?"
+    pit1Label = "the US government"
+    pit2Label = "US scientists"
+    pit3Label = "foreign governments"
+    pit4Label = "foreign scientists"
+    pit5Label = "the Intergovernmental Panel on Climate Change (IPCC)"
+
+
+    # Knowledge Question from Allianz Climate Literacy Report
+    cck_title = 'What do you know about climate change?'
+    cck_header = 'Please answer all these questions to the best of your knowledge, without looking up any information. You may choose the "I do not know" option if you are unsure or do not know'
+    know_dontknow = "I don't know."
+
+    know_1qu = "What is the COP?" ## This is maybe too easy given that the whole study is about CC???
+    know_1a = "UN initiative for distributing funds to reduce the impact of climate change on poverty."
+    know_1b = "An annual formal meeting to discuss climate change and establish mitigation actions."
+    know_1c = "An EU initiative against organized and war crimes." 
+    
+    know_2qu = "What does Net-Zero mean?"
+    know_2a = "Monetary strategy of increasing interest rate to fight inflation."
+    know_2b = "No greenhouse gas emission by a specific date, typically 2050."
+    know_2c = "Carbon emission neutrality, stabilization of greenhouse gas concentrations in the atmosphere."
+    
+    know_3qu = "The Intergovernmental Panel on Climate Change (IPCC) plays an important role in global climate policy. Which one?"
+    know_3a = "Providing objective scientific information relevant to understanding climate change."
+    know_3b = "Deciding on global climate policies, particularly setting the global carbon price."
+    know_3c = "Host of the UN climate justice court which arbitrates climate disputes between states."
+    
+    know_4qu = "What is the carbon market?"
+    know_4a = "The supply channel of the EU backed gas-buying cartel that aims to supply affordable natural gas to EU countries struggling to get supply because of the war in Ukraine."
+    know_4b = "A trading system through which emitters may buy or sell units of greenhouse-gas emission allowances to meet national restrictions on total emissions."
+    know_4c = "An online marketplace where you can buy recycled carbon fiber and carbon black."
+    
+
+    ## I suggest a modification of tis question:
+    #Rising temperatures pose no existential threat. Even if the rise exceeds 3°C, humans and nature can adapt. Do you agree with this statement?"
+    know_5qu = "How long does CO2 stay in the atmosphere?"
+    know_5a = "10 years"
+    know_5b = "up to 1000 years"
+    know_5c = "more than 5000 years"
+
+    
+    ## I suggest a modification of this question
+    know_6qu = "Which of the following options describes the greenhouse gas effect"
+    # old was Climate change cannot be stopped. Average temperatures will continue to increase in the near future. The only thing we can possible do is to limit the increase to 1.5°C."
+    know_6a = "Deforestation and plastic pollution cause a collapse of many ecosystems. The increasing loss of biodiversity and the loss of flora and fauna in the wild is called the greenhouse gas effect."
+    know_6b = "Greenhouse gasses cause air pollution. They lead to more fine particulars and more fine dust which in turn decreases the ventilation. Without the circulation of fresh air, the earth gets increasingly warmer. "
+    know_6c = "Excess greenhouse gases accumulate in the atmosphere. Because of their molecular structure, the infrared radiation from the earth is reflected and is re-radiated to the earth. This way, heat is trapped."
+    
+    
+
+    know_8qu = "At current rates, after how many years we will have burnt our CO2-budget to limit the temperature rise to 1.5C?" # this will be 2029 (Stand 2023)
+    know_8a = "5 years"
+    know_8b = "10 years"
+    know_8c = "23 years"
+    
+
+    know_9qu = "Which country/region causes the highest absolute CO2-emissions per year?"
+    know_9a = "China"
+    know_9b = "USA"
+    know_9c = "EU"
+    know_9d = "India"
+
+    know_10qu = "Which of the four countries/regions causes the highest per-capita CO2-emissions per year?"
+    know_10a = "China"
+    know_10b = "USA"
+    know_10c = "EU"
+    know_10d = "India"
+    
+
+
+    ### Demographics
+    demographics_title = "Personal data"
+    demographics_header = "Please enter the following information about yourself."
+
+    age_label = "How old are you?"
+    gender_label = "What gender do you identify as?"
+    female = "Female"
+    male = "Male"
+    diverse = "Diverse"
+    other = "Other"
+
+    income_label = "What is your yearly income?"
+    income_less_than_A = "Less than $25,000"
+    income_A_to_B = "$25,000 - $50,000"
+    income_B_to_C = "$50,001 - $75,000"
+    income_C_to_D = "$75,001 - $100,000"
+    income_more_than_D = "More than $100,000"
+    prefer_not_to_say = "Prefer not to say"
+
+    education_label = "What is your highest level of education?"
+    high_school = "High School"
+    some_college = "Some College"
+    bachelors_degree = "Bachelor's Degree"
+    masters_degree = "Master's Degree"
+    doctoral_degree = "Doctoral Degree"
+
+    residential_area_label = "What describes your residential area best?"
+    metropolitan_area = "Metropolitan Area"
+    suburban = "Suburban Area"
+    rural = "Rural Area"
+
+    zip_code_label = "Zip Code (voluntarily)"
+    
+    party_affiliation_label = "Which party would you be most likely to vote for?"
+    republicans = "Republicans"
+    democrats = "Democrats"
+    independent_party = "Independent Party"
+    other_party = "Other"
+
+    # Transition page
+    Transition_text = "Thank you for your participation so far! <br> You have now completed the policy task and will continue to answer some questions about your attitudes and perceptions. "
+    
+    # goodbye page 
+    goodbye_text = "Thank you for your participation in our study! <br> You are now at the end of the study and will be redirected for compensation."
+
+
+    ## for pre-test Jessi only, Ninas items
+
+    Additional_title=  'Some additional questions'
+    Additional_intro=	'Lastly we would like to ask you some additional questions.'
+	
+    familiar_header	= 'For each of these actions please indicate how familiar you are with them:'
+    familiar_label1= 'choosing your own electricity contract or switching to a different more renewable contract'
+    familiar_label2	= 'following a vegetarian diet (i.e eating no meat including fish)'
+    familiar_label3	= 'commuting to work by bike'
+    familiar_label4	= 'buying and consuming imported foods (i.e. from other countries)'
+    familiar_label5	= 'buying and consuming regional foods (from your area / country)'
+    familiar_label6	= 'recycling (i.e. paper, glass, or metals)'
+    familiar_label7	= 'traveling by train to go on vacations'
+    familiar_label8	= 'traveling by bus to go on vacations'
+    familiar_label9	= 'Traveling by plane to go on vacations'
+    familiar_A	= 'very unfamiliar'
+    familiar_B	= 'unfamiliar'
+    familiar_C	= 'somewhat unfamiliar'
+    familiar_D	= 'neither unfamiliar nor familiar'
+    familiar_E	= 'somewhat familiar'
+    familiar_F	= 'familiar'
+    familiar_G	= 'very familiar'
+	
+    Difficult_header = 'Please indicate how easy or difficult it would be for an average person in your country to perform the following actions'
+    difficult_label1 = 'choose or switch their electricity contract (e.g., to have a higher share of electricity from renewable sources)'
+    difficult_label2 = 'follow a vegetarian diet (i.e. not to eat meat or fish)'
+    difficult_label3 ='commute to work by bike'
+    difficult_label4 = 'buy and consume imported foods (i.e. from other countries)'
+    difficult_label5 = 'buy and consume mostly regional foods (from your area / country)'
+    difficult_label6 = 'recycle (i.e. paper, glass, or metals)'
+    difficult_label7 = 'take the train to go on vacations'
+    difficult_label8 = 'take the bus to go on vacations'
+    difficult_label9 = 'take the plane to go on vacations'
+	
+    difficult_A = 'very difficult'
+    difficult_B = 'difficult'
+    difficult_C = 'somewhat difficult'
+    difficult_D = 'neither difficult nor easy'
+    difficult_E	= 'somewhat easy'
+    difficult_F	= 'easy'
+    difficult_G	= 'very easy'
---
+++ Scales_Jessi/lexicon_zh_hans.py
@@ -0,0 +1,254 @@
+class Lexicon:
+    #Scale anchors 
+    strongly_liberal = "非常开明"
+    somewhat_liberal = "有点开明"
+    moderate = "不偏不倚"
+    somewhat_conservative = "有点保守"
+    strongly_conservative = "非常保守"
+
+    opposed_to_values = "违背我的价值观"
+    not_important = "不重要"
+    extremely_important = "极其重要"
+
+    not_at_all = "一点也不"
+    very_much = "非常"
+
+    #Climate Change Concern Tobler
+    ccc_title = "对气候变化的态度"
+    ccc_header = "您对以下说法的同意程度如何？"
+    ccc1Label = "我们必须保护气候的微妙平衡。"
+    ccc2Label = "气候保护对我们的未来很重要。"
+    ccc3Label = "我担心气候状况。"
+    ccc4Label = "气候变化对人类和自然造成严重后果。"
+    ccc10Label = "媒体夸大了气候变化及其后果。"
+    ccc11Label = "气候变化是一场闹剧。"
+    ccc12Label = "只要气象学家无法准确预测天气，气候也无法可靠预测。"
+    ccc13Label = "还有比气候保护更大的问题。"
+    ccc14Label = "我没有感受到气候变化的威胁。"
+    ccc15Label = "气候变化的影响是不可预测的；因此，我对气候有益的行为是徒劳的。"
+    ccc16Label = "气候保护不必要地阻碍了经济增长。"
+
+    #Climate Change Emotion Knauf/Truelove
+    cce_title = "对气候变化的态度"
+    cce_header = "当我想到气候变化时，我感觉…"
+    cce1Label = "愤怒"
+    cce2Label = "害怕"
+    cce3Label = "悲伤"
+    cce4Label = "喜悦"
+    cce5Label = "好奇"
+    cce6Label = "希望"
+
+        # new emotions
+    #Climate Change Emotion
+
+    cceA1Label = "angry"
+    cceA2Label = "mad"
+    cceA3Label = "irritated"
+    cceF1Label = "fearful"
+    cceF2Label = "afraid"
+    cceF3Label = "scared"
+    cceS1Label = "sad"
+    cceS2Label = "sorrowful"
+    cceS3Label = "unhappy"
+    cceH1Label = "hopeful"
+    cceH2Label = "optimistic"
+    cceH3Label = "upbeat"
+    cceG1Label = "guilty"
+    cceG2Label = "regretful"
+    cceG3Label = "remorseful"
+
+    # Personal efficacy Leiserowitz et al, 2010
+    #Only item 1
+    pe_title = "Attitudes about Climate Change"
+    pe_header = "How much do you agree with the following statements?"
+    pe1Label = "The actions of a single individual won't make any difference in climate change."
+  
+    
+    # Worldviews and values - Hierarchy-Egalitarianism & Individualism-Communitarianism  
+    ## TBD kevin tam
+    wvv_title = "How do you see the world?"
+    wvv_header = "How much do you agree with the following statements?"
+    wvv1Label = "Our society would be better off if the distribution of wealth was more equal."
+    wvv2Label = "A lot of problems in our society come from the decline in the traditional family, where the man works and the woman stays home."
+    wvv3Label = "Discrimination against minorities is still a very serious problem in our country."
+    wvv4Label = "The government interferes far too much in our everyday lives."
+    wvv5Label = "I feel that people who are successful in business have a right to enjoy their wealth as they see fit."
+    wvv6Label = "Too many people expect society to do things for them that they should be doing for themselves."
+
+   # Importance of biospheric values van der Linden, 2015   
+    ibv_title = "环境问题的指导原则"
+    ibv_header = "以下价值作为你生活的指导原则对你来说有多重要？"
+    ibv1Label = "尊重地球（与其他物种和谐相处"
+    ibv2Label = "保护环境（保全自然"
+    ibv3Label = "防止污染（保护自然资源"
+    ibv4Label = "与自然合一（融入自然"
+
+    # Political Orientation Pennycook et al 2020
+    po_title = "Political Orientation"
+    po_header = "What's your general stance in political issues?"
+    po1Label = "On social issues I am..."
+    po2Label = "On economic issues I am..."
+
+    # Trust  in institutions in terms of cc Based on (Pan et al., 2023)
+    pit_title = "Trust in regards to climate change"
+    pit_header = "How much do you trust these actors in climate change?"
+    pit1Label = "the US government"
+    pit2Label = "US scientists"
+    pit3Label = "foreign governments"
+    pit4Label = "foreign scientists"
+    pit5Label = "the Intergovernmental Panel on Climate Change (IPCC)"
+
+    # Knowledge Question from Allianz Climate Literacy Report
+    cck_title = 'What do you know about climate change?'
+    cck_header = 'Please answer all these questions to the best of your knowledge, without looking up any information. You may choose the "I do not know" option if you are unsure or do not know'
+    know_dontknow = "I don't know."
+
+    know_1qu = "What is the COP?" ## This is maybe too easy given that the whole study is about CC???
+    know_1a = "UN initiative for distributing funds to reduce the impact of climate change on poverty."
+    know_1b = "An annual formal meeting to discuss climate change and establish mitigation actions."
+    know_1c = "An EU initiative against organized and war crimes." 
+    
+    know_2qu = "What does Net-Zero mean?"
+    know_2a = "Monetary strategy of increasing interest rate to fight inflation."
+    know_2b = "No greenhouse gas emission by a specific date, typically 2050."
+    know_2c = "Carbon emission neutrality, stabilization of greenhouse gas concentrations in the atmosphere."
+    
+    know_3qu = "The Intergovernmental Panel on Climate Change (IPCC) plays an important role in global climate policy. Which one?"
+    know_3a = "Providing objective scientific information relevant to understanding climate change."
+    know_3b = "Deciding on global climate policies, particularly setting the global carbon price."
+    know_3c = "Host of the UN climate justice court which arbitrates climate disputes between states."
+    
+    know_4qu = "What is the carbon market?"
+    know_4a = "The supply channel of the EU backed gas-buying cartel that aims to supply affordable natural gas to EU countries struggling to get supply because of the war in Ukraine."
+    know_4b = "A trading system through which emitters may buy or sell units of greenhouse-gas emission allowances to meet national restrictions on total emissions."
+    know_4c = "An online marketplace where you can buy recycled carbon fiber and carbon black."
+    
+
+    ## I suggest a modification of tis question:
+    #Rising temperatures pose no existential threat. Even if the rise exceeds 3°C, humans and nature can adapt. Do you agree with this statement?"
+    know_5qu = "How long does CO2 stay in the atmosphere?"
+    know_5a = "10 years"
+    know_5b = "up to 1000 years"
+    know_5c = "more than 5000 years"
+
+    
+    ## I suggest a modification of this question
+    know_6qu = "Which of the following options describes the greenhouse gas effect"
+    # old was Climate change cannot be stopped. Average temperatures will continue to increase in the near future. The only thing we can possible do is to limit the increase to 1.5°C."
+    know_6a = "Deforestation and plastic pollution cause a collapse of many ecosystems. The increasing loss of biodiversity and the loss of flora and fauna in the wild is called the greenhouse gas effect."
+    know_6b = "Greenhouse gasses cause air pollution. They lead to more fine particulars and more fine dust which in turn decreases the ventilation. Without the circulation of fresh air, the earth gets increasingly warmer. "
+    know_6c = "Excess greenhouse gases accumulate in the atmosphere. Because of their molecular structure, the infrared radiation from the earth is reflected and is re-radiated to the earth. This way, heat is trapped."
+    
+    
+
+    know_8qu = "At current rates, after how many years we will have burnt our CO2-budget to limit the temperature rise to 1.5C?" # this will be 2029 (Stand 2023)
+    know_8a = "5 years"
+    know_8b = "10 years"
+    know_8c = "23 years"
+    
+
+    know_9qu = "Which country/region causes the highest absolute CO2-emissions per year?"
+    know_9a = "China"
+    know_9b = "USA"
+    know_9c = "EU"
+    know_9d = "India"
+
+    know_10qu = "Which of the four countries/regions causes the highest per-capita CO2-emissions per year?"
+    know_10a = "China"
+    know_10b = "USA"
+    know_10c = "EU"
+    know_10d = "India"
+    
+    ### Demographics
+    demographics_title = "个人数据" 
+    demographics_header = "请输入以下有关您自己的信息。"
+
+    age_label = "你今年多大？"
+    gender_label = "您的性别是什么？" 
+    female = "女性"
+    male = "男性"
+    other = "其他"
+
+    income_label = "你的年收入是多少？"
+    income_less_than_A = "3万元人民币以下"
+    income_A_to_B = "30,000 - 60,000元人民币"
+    income_B_to_C = "60,001 - 120,000元人民币"
+    income_C_to_D = "120,001 - 300,000元人民币"
+    income_D_to_E = "300,001 - 500,000元人民币"
+    income_more_than_E = "超过50万元人民币"
+    prefer_not_to_say = "不愿意说"
+
+    education_label = "你的最高学历是什么？"
+    high_school = "初中"
+    vocational_education = "职业教育"
+    some_college = "高等教育（职业）"
+    bachelors_degree =  "学士学位"
+    masters_degree = "硕士学位"
+    doctoral_degree = "博士学位"
+
+    residential_area_label = "什么最能描述您的居住区？"
+    metropolitan_area = "大都市区"
+    suburban = "郊区"
+    rural = "农村"
+    
+    #not relevant for China 
+    party_affiliation_label = "Which party would you be most likely to vote for?"
+    republicans = "Republicans"
+    democrats = "Democrats"
+    independent_party = "Independent Party"
+    other_party = "Other"
+
+    zip_code_label = "邮政编码（自愿）"
+
+
+    # Transition page
+    Transition_text = "感谢您迄今为止的参与！<br>您现在已经完成了政策任务，并将继续回答一些有关您的态度和看法的问题。" 
+    # goodbye page 
+    goodbye_text = "感谢您参与我们的研究！<br>您现在已完成研究，将被重新导引以获取酬金。"  
+
+    Additional_title = "一些额外的问题"
+    Additional_intro = "最后我们想问你一些额外的问题。"
+
+    familiar_header = "对于每项操作，请表明您对它们的熟悉程度："
+    familiar_label1 = "选择自己的电力合同或改用其他更具可再生性的合同"
+    familiar_label2 = "遵循素食（即不吃肉，包括鱼）"
+    familiar_label3 = "骑自行车上班"
+    familiar_label4 = "购买和食用进口食品（即来自其他国家）"
+    familiar_label5 = "购买和消费区域食品（来自您所在的地区/国家）"
+    familiar_label6 = "回收（即纸张、玻璃或金属）"
+    familiar_label7 = "乘火车去度假"
+    familiar_label8 = "乘公共汽车去度假"
+    familiar_label9 = "乘飞机去度假"
+    familiar_A = "非常陌生"
+    familiar_B = "陌生"
+    familiar_C = "有点陌生"
+    familiar_D = "不陌生也不熟悉"
+    familiar_E = "有点熟悉"
+    familiar_F = "熟悉的"
+    familiar_G = "非常熟悉"
+
+    Difficult_header = "请说明对于您所在国家/地区的普通人来说执行以下操作有多容易或多困难"
+    difficult_label1 = "选择或转换他们的电力合同（例如，获得更高比例的可再生能源电力）"
+    difficult_label2 = "遵循素食（即不吃肉或鱼）"
+    difficult_label3 = "骑自行车上班"
+    difficult_label4 = "购买和消费进口食品（即来自其他国家）"
+    difficult_label5 = "主要购买和消费区域食品（来自您所在的地区/国家）"
+    difficult_label6 = "回收（即纸张、玻璃或金属）"
+    difficult_label7 = "坐火车去度假"
+    difficult_label8 = "坐公交车去度假"
+    difficult_label9 = "坐飞机去度假"
+
+    difficult_A = "非常困难"
+    difficult_B = "难的"
+    difficult_C = "有点困难"
+    difficult_D = "既不困难也不容易"
+    difficult_E = "有点容易"
+    difficult_F = "简单的"
+    difficult_G = "非常简单"
+
+
+
+
+    
+    
+
