ba3087512aeadf5d67fdf9d7c15623899b472907
Nina Frings <nina.frings@unibas.ch>
2024-03-11 17:41:42 +0100

    update task based on Kristians feedback

--- Nina_carbontask/lexicon_de.py
+++ Nina_carbontask/lexicon_de.py
@@ -10,6 +10,6 @@ class Lexicon:
 
     BEHAVIORTYPES =  '["Ernährung", "Elektrizität", "Recycling", "Lebensmittel", "Arbeitsweg", "Urlaub"]'
     instruction_task ='Stellen Sie sich eine Person mit dem folgenden Lebensstil vor. Wie groß ist Ihrer Meinung nach der CO2-Fußabdruck dieser Person?'
-    task_info = 'Bitte beachten Sie, dass ein kleiner Fußabdruck sich auf eine Person bezieht, deren Lebensstil mit geringen Treibhausgasemissionen einhergeht (für eine Person in Deutschland) und ein großer Fußabdruck auf eine Person, deren Lebensstil mit vielen Treibhausgasemissionen einhergeht. <br> Bitte machen Sie Ihre Bewertung von 1 - kleiner CO2-Fußabdruck bis 10 - großer CO2-Fußabdruck basierend auf den 6 vorgestellten Verhaltensweisen'
+    task_info = 'Bitte beachten Sie, dass ein kleiner Fußabdruck sich auf eine Person bezieht, deren Lebensstil mit geringen Treibhausgasemissionen einhergeht. <br> Bitte machen Sie Ihre Bewertung von 1 - kleiner CO2-Fußabdruck bis 10 - großer CO2-Fußabdruck basierend auf den 6 vorgestellten Verhaltensweisen.<br> Auf der Skala von 1 bis 10 entspricht jeder Anstieg um 1 (z. B. von 2 auf 3 oder von 7 auf 8) einem Anstieg von etwa 500 kg Kohlendioxid.'
     # 
      
