a9be13e01b8fb5616a7218b4ff4a9200b7aa670a
Nina Frings <nina.frings@unibas.ch>
2024-03-18 17:28:43 +0100

    adaptation chinese sentence

--- Jessi_Instructions/lexicon_zh_hans.py
+++ Jessi_Instructions/lexicon_zh_hans.py
@@ -20,7 +20,7 @@ class Lexicon:
     transition_title = '欢迎来到研究的下一个部分'
     transitions_second_1 =  '您已完成本研究四个部分中的 '
     transitions_second_2 = '部分。第 '
-    transitions_second_3 = '部分即将开始。 如果您在继续学习之前需要短暂休息，现在是个好时机。 '
+    transitions_second_3 = '部分即将开始。如果你在继续这项研究之前需要短暂休息，现在是个好时机。'
     transitions_second_4 = '请尽量一次性完成下一部分，不要中断。感谢您抽出时间参与本研究。 准备就绪后，请点击 "下一个"。'
 
     
