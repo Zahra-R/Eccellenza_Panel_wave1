27a72e52db43c4b6036e0fb5d6ec4beb6068a7dd
Nina Frings <nina.frings@unibas.ch>
2024-03-19 17:04:09 +0100

    weiter buttons rechts

--- Nina_carbontask/lexicon_en.py
+++ Nina_carbontask/lexicon_en.py
@@ -7,4 +7,5 @@ class Lexicon:
     title = 'Please rate these carbon footprints'
     BEHAVIORTYPES =  '["Diet", "Electricity", "Recycling", "Food", "Commute", "Vacation"]'
     instruction_task ='<b> Think about a person with the lifestyle below. How large do you think the carbon footprint of this person is? </b>'
-    task_info = 'Please note that a small footprint refers to a person that has a lifestyle that is accompanied by little greenhouse gas emissions. <br> Please make your rating from 1– small carbon footprint to 10– large carbon footprint based on the 6 behaviors presented. <br> On the scale of 1 to 10, each increase of 1 (e.g., from 2 to 3 or from 7 to 8) corresponds to an increase of about 500 kg of carbon dioxide.'
+    task_info = '<b> On the scale of 1 to 10, each increase of 1 (e.g., from 2 to 3 or from 7 to 8) corresponds to an increase of about 500 kg of carbon dioxide. </b> <br> Please note that a small footprint refers to a person that has a lifestyle that is accompanied by little greenhouse gas emissions. <br> Please make your rating from 1– small carbon footprint to 10– large carbon footprint based on the 6 behaviors presented.'
+
